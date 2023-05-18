b3089c2320cc5cac8e4f77c44229f69a7024011e
Charan Thoutam <thoutamsricharan@gmail.com>
2023-05-18 19:37:18 +0530

    initial commit

--- Trees/binary_tree.py
+++ Trees/binary_tree.py
@@ -42,7 +42,7 @@ class BinaryTreeNode:
         else:
             # val is in right subtree
             if self.right:
-                # val is in left subtree of left subtree
+                # val is in right of right subtree
                 return self.right.search(val)
             else:
                 # val doesn't exist in the tree
@@ -97,6 +97,22 @@ class BinaryTreeNode:
 
         return elements
     
+    def find_min(self):
+        if self.left == None:
+            # if there is no left subtree to the current node i.e., self.data
+            return self.data
+        else:
+            # if there is left subtree to the current node
+            return self.left.find_min()
+        
+    def find_max(self):
+        if self.right == None:
+            return self.data
+        else:
+            return self.right.find_max()
+        
+        
+    
 def build_tree(elements):
     print("Elements in the list: ", elements)
     root = BinaryTreeNode(elements[0]) # added root node
@@ -113,11 +129,16 @@ if __name__ == '__main__':
 
     # print(countries_build_tree.search("USA"))
 
-    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34, 1]
     build_num_tree = build_tree(numbers)
 
     print("\nIn order traversal: ", build_num_tree.in_order())
     print("Pre order traversal: ", build_num_tree.pre_order())
     print("Post order traversal: ", build_num_tree.post_order())
 
-    # print(build_num_tree.search(17))
+    print(build_num_tree.search(17)) # Search for the element and give output True or Flase
+
+    print("Sum of elements: ",sum(set(numbers))) # here set() function removes duplicate elements i.e., 1 repeated two times
+
+    print("Maximum value in tree: ", build_num_tree.find_max()) # returns maximum value
+    print("Minumum value in tree: ", build_num_tree.find_min()) # returns minimum value
